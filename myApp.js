04222c6e1cdafecd166888d032fc91f58e626ecd
fish <fisseha.137@gmail.com>
2022-08-18 14:33:24 +0300

    added the find by name functionlity

--- myApp.js
+++ myApp.js
@@ -70,8 +70,12 @@ const createManyPeople = (arrayOfPeople, done) => {
   };
 };
 
+// find a person by his name
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({name: personName}, function (err, personFound) {
+    if (err) return console.log(err);
+    done(null, personFound);
+  });
 };
 
 const findOneByFood = (food, done) => {
